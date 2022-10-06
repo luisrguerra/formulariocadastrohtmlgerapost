405631510cc19379fe56eba2201e0ff854c250ca
luisrguerra <luiseduardoribeiroguerra@gmail.com>
2022-10-05 23:58:15 -0300

    novo renderizador

--- cadastro/adm/renderizar.js
+++ cadastro/adm/renderizar.js
@@ -18,3 +18,24 @@ function btnSalvar(){
         } 
     });      
 };
+
+
+function btnSalvar2(id){
+  var elemento = document.getElementById("previewBox")
+  const zoom_tmp = elemento.style.zoom;
+  elemento.style.zoom = "100%";
+
+  htmlToImage.toPng(elemento)
+    .then(function (dataUrl) {
+      var link = document.createElement('a');
+      link.href = dataUrl;
+      link.download = 'imagem.png';
+      link.click();
+      elemento.style.zoom = zoom_tmp;
+    })
+    .catch(function (error) {
+      console.error('Erro renderizador', error);
+      elemento.style.zoom = zoom_tmp;
+    });
+
+};
